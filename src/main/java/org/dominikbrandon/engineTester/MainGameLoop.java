8330c1b1c08cedf944884986d41dc5eeaeb46632
Dominik Grzelak <grzelak.dominik@gmail.com>
2018-05-27 22:02:21 +0200

    added mechanisms for projection and view matrices, replaced 2d object with a cube, added basic camera movement

--- src/main/java/org/dominikbrandon/engineTester/MainGameLoop.java
+++ src/main/java/org/dominikbrandon/engineTester/MainGameLoop.java
@@ -1,5 +1,7 @@
 package org.dominikbrandon.engineTester;
 
+import org.dominikbrandon.entites.Camera;
+import org.dominikbrandon.entites.Entity;
 import org.dominikbrandon.models.TexturedModel;
 import org.dominikbrandon.renderEngine.DisplayManager;
 import org.dominikbrandon.renderEngine.Loader;
@@ -9,6 +11,7 @@ import org.dominikbrandon.shaders.StaticShader;
 import org.dominikbrandon.textures.ModelTexture;
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
+import org.lwjgl.util.vector.Vector3f;
 
 import java.io.IOException;
 
@@ -17,34 +20,118 @@ public class MainGameLoop {
     public static void main(String[] args) throws LWJGLException, IOException {
         DisplayManager.createDisplay();
         Loader loader = new Loader();
-        Renderer renderer = new Renderer();
         StaticShader shader = new StaticShader();
+        Renderer renderer = new Renderer(shader);
 
         float[] vertices = {
-                -0.5f, 0.5f, 0f,
-                -0.5f, -0.5f, 0f,
-                0.5f, -0.5f, 0f,
-                0.5f, 0.5f, 0f
+                -0.5f,0.5f,-0.5f,
+                -0.5f,-0.5f,-0.5f,
+                0.5f,-0.5f,-0.5f,
+                0.5f,0.5f,-0.5f,
+
+                -0.5f,0.5f,0.5f,
+                -0.5f,-0.5f,0.5f,
+                0.5f,-0.5f,0.5f,
+                0.5f,0.5f,0.5f,
+
+                0.5f,0.5f,-0.5f,
+                0.5f,-0.5f,-0.5f,
+                0.5f,-0.5f,0.5f,
+                0.5f,0.5f,0.5f,
+
+                -0.5f,0.5f,-0.5f,
+                -0.5f,-0.5f,-0.5f,
+                -0.5f,-0.5f,0.5f,
+                -0.5f,0.5f,0.5f,
+
+                -0.5f,0.5f,0.5f,
+                -0.5f,0.5f,-0.5f,
+                0.5f,0.5f,-0.5f,
+                0.5f,0.5f,0.5f,
+
+                -0.5f,-0.5f,0.5f,
+                -0.5f,-0.5f,-0.5f,
+                0.5f,-0.5f,-0.5f,
+                0.5f,-0.5f,0.5f
+
         };
-        int[] indices = {
-                0,1,3,
-                3,1,2
-        };
+
         float[] textureCoords = {
                 0,0,
                 0,1,
                 1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
                 1,0
+
+
         };
+
+        int[] indices = {
+                0,1,3,
+                3,1,2,
+                4,5,7,
+                7,5,6,
+                8,9,11,
+                11,9,10,
+                12,13,15,
+                15,13,14,
+                16,17,19,
+                19,17,18,
+                20,21,23,
+                23,21,22
+
+        };
+//        float[] vertices = {
+//                -0.5f, 0.5f, 0f,
+//                -0.5f, -0.5f, 0f,
+//                0.5f, -0.5f, 0f,
+//                0.5f, 0.5f, 0f
+//        };
+//        int[] indices = {
+//                0,1,3,
+//                3,1,2
+//        };
+//        float[] textureCoords = {
+//                0,0,
+//                0,1,
+//                1,1,
+//                1,0
+//        };
 
         RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
         ModelTexture texture = new ModelTexture(loader.loadTexture("house"));
-        TexturedModel texturedModel = new TexturedModel(model, texture);
+        TexturedModel staticModel = new TexturedModel(model, texture);
+        Entity entity = new Entity(staticModel, new Vector3f(0,0,-10),0,0,0,1);
+        Camera camera = new Camera();
 
         while(!Display.isCloseRequested()) {
+//            entity.increasePosition(0, 0, -0.01f);
+            entity.increaseRotation(1,1,0);
+            camera.move();
+
             renderer.prepare();
             shader.start();
-            renderer.render(texturedModel);
+            shader.loadViewMatrix(camera);
+            renderer.render(entity, shader);
             shader.stop();
             DisplayManager.updateDisplay();
         }
--- src/main/java/org/dominikbrandon/shaders/StaticShader.java
+++ src/main/java/org/dominikbrandon/shaders/StaticShader.java
@@ -1,18 +1,45 @@
 package org.dominikbrandon.shaders;
 
+import org.dominikbrandon.entites.Camera;
+import org.dominikbrandon.toolbox.Maths;
+import org.lwjgl.util.vector.Matrix4f;
+
 import java.io.IOException;
 
 public class StaticShader extends ShaderProgram {
     private static final String VERTEX_FILE = "src/main/java/org/dominikbrandon/shaders/vertexShader.txt";
     private static final String FRAGMENT_FILE = "src/main/java/org/dominikbrandon/shaders/fragmentShader.txt";
+    private int locationTransformationMatrix;
+    private int locationProjectionMatrix;
+    private int locationViewMatrix;
 
     public StaticShader() throws IOException {
         super(VERTEX_FILE, FRAGMENT_FILE);
     }
 
     @Override
+    protected void getAllUniformLocations() {
+        locationTransformationMatrix = super.getUniformLocation("transformationMatrix");
+        locationProjectionMatrix = super.getUniformLocation("projectionMatrix");
+        locationViewMatrix = super.getUniformLocation("viewMatrix");
+    }
+
+    @Override
     protected void bindAttributes() {
         super.bindAttribute(0, "position");
         super.bindAttribute(1, "textureCoords");
+    }
+
+    public void loadTransformationMatrix(Matrix4f matrix) {
+        super.loadMatrix(locationTransformationMatrix, matrix);
+    }
+
+    public void loadViewMatrix(Camera camera) {
+        Matrix4f viewMatrix = Maths.createViewMatrix(camera);
+        super.loadMatrix(locationViewMatrix, viewMatrix);
+    }
+
+    public void loadProjectionMatrix(Matrix4f projection) {
+        super.loadMatrix(locationProjectionMatrix, projection);
     }
 }
